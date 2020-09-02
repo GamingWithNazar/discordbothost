24587282de4efc122a41234d5b9b785c825ddbe8
GamingWithNazar <nazar.daniliuk@gmail.com>
2020-09-02 15:34:05 -0700

    Update index.js

--- index.js
+++ index.js
@@ -37,10 +37,31 @@ bot.on('ready', () =>{
 
 //Custom Status
 
-Client.on('ready', async () => {
-    await client.user.setPresence({ game: { name: 'people say !help' }, status: 'watching' });
- });
- 
+const db = require("quick.db")
+const discord = require("discord.js")
+
+module.exports = {
+  name: "status",
+  description: "Change the bot status",
+  usage: "status <here>",
+  category: "owner",
+  run: async (client, message, args) => {
+    
+    //OWNER ONLY COMMAND
+    if(!message.author.id === "710971859109281822") {
+      return message.channel.send("This command can only be used by owner")
+    }
+    //ARGUMENT
+    if(!args.length) {
+      return message.channel.send("Please give status message")
+    }
+    
+ db.set(`status`, args.join(" "))
+   await message.channel.send("Updated the bot status")
+    process.exit(1);
+    
+  }
+}
 //Commands
 
 bot.on('message', message=>{
