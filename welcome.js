7c149d7c754facb342035020188cc738aee04821
GamingWithNazar <nazar.daniliuk@gmail.com>
2020-09-09 22:04:50 -0700

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -2,7 +2,7 @@ module.exports = client => {
 
     const channelId = '753463674349420694'
 
-    client.on('guildMemberAdd', member => {
+    bot.on('guildMemberAdd', member => {
         console.log(member)
 
         const messsage = `Hello <@${member.id}> please read #rules to know the rules of the server!`
